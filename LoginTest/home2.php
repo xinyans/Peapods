17c438fa91fc10b7a100992d53900b0b0dbfa33b
xinyans <sunx15@rpi.edu>
2019-10-26 01:02:40 -0400

    ReadMe and other minor Changes

--- LoginTest/home2.php
+++ LoginTest/home2.php
@@ -3,7 +3,7 @@
   <head>
     <title>PeaPods: Home</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-    <link rel="stylesheet" href="test.css" type="text/css" />
+    <link rel="stylesheet" href="style.css" type="text/css" />
   </head>
 
   <body>
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # websys
 Group 2 - Peapods
 
+Reference:
+1. https://brandpalettes.com/twitter-colors/
+2. https://www.w3.org/Style/Examples/007/evenodd.en.html
+3. https://www.computerhope.com/issues/ch001704.htm
