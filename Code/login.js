2d90ed3f05d2b7242d2cd02ba32bde1e97a64206
Nick Meyer <meyern4@rpi.edu>
2019-10-27 14:06:46 -0400

    Finished login

--- Code/login.js
+++ Code/login.js
@@ -1,3 +1,43 @@
+var login = `
+<aside class = "login">
+    <form onsubmit="false">
+        <button type="button">X</button>
+        <input type="text" value = "Username">
+        <input type="text" value = "Password">
+        <button type="button">Submit</button>
+    </form>
+</aside>
+`;
+
+function loginClick(){
+    temp = $("body").html();
+    $("body").html(login + temp);
+    $("body>aside>form>button:nth-child(1)").click(function(){
+        $("body").html(temp);
+        $("nav>header>img:nth-child(2)").click(loginClick);
+        $("nav>header>img:nth-child(2)").attr("src", "Resources/loginguy.png");
+    });
+    $("body>aside>form>button:nth-child(4)").click(function(){
+        loginData = {
+            "user" : $("body>aside>form>input:nth-child(2)").val(),
+            "pass" : $("body>aside>form>input:nth-child(3)").val()
+        }
+        if(!(loginData["user"] == "" || loginData["pass"] == "")){
+            $("body").html(temp);
+            $("nav>header>img:nth-child(2)").attr("src", "Resources/door.png");
+            $("nav>header>img:nth-child(2)").click(logoutClick);
+        }
+    });
+}
+
+function logoutClick(){
+    
+}
+
+function addLoginListeners(){
+    $("nav>header>img:nth-child(2)").click(loginClick);
+}
+
 window.onload = function() {
-    this.console.log("heererasdasdas");
+    this.addLoginListeners();
 }
