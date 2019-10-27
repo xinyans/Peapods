87abc6016ea41881a9a7116c82e9f412c60e7c04
Peter Gramenides <30732292+petergramenides@users.noreply.github.com>
2019-10-27 15:24:29 -0400

    draft slideText

---
+++ Code/index.js
@@ -0,0 +1,14 @@
+var actionWords = ["find roommates.", "organize groups.", "find opportunities."];
+var intervalID = window.setInterval(slideText, 3000);
+var index = 0;
+
+function slideText()
+{
+	document.getElementById("slideText").innerHTML = actionWords[index];
+	index++;
+	if (index == 3)
+	{
+		index = 0;
+	}
+}
+
--- index.php
+++ index.php
@@ -3,9 +3,11 @@
     <head>
         <meta charset = "UTF-8">
         <title>Prototype</title>
-        <link rel="icon" type="image/png" href="resources/favicon.png">
+        <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="Stylesheets/stylesheets.css">
+        <link rel="stylesheet" type="text/css" href="Stylesheets/index.css">
         <script src="code/jquery-3.4.1.min.js"></script>
+        <script src="code/index.js"></script>
         <script src="code/login.js"></script>
     </head>
     <body>
@@ -20,17 +22,19 @@
 					<a href="https:\\www.google.com">Create</a>
 					<a href="https:\\www.google.com">Dashboard</a>
 				</nav>
-				<input type="text" name="search" placeholder ="Search">
+				<input type="text" name="search" value ="Search">
             </section>
         </nav>
         <main>
 			<article>
 				<header>
-					<img src="Resources/homeBackground.jpg">
+					<img id="background" src="Resources/homeBackground.png">
+                    <h1 id="startText">We help people</h1>
+                    <h1 id="slideText">organize courses.</h1>
 				</header>
-                	
+ 
                 <section>
-                    
+
                 </section>
             </article>
 		</main>
