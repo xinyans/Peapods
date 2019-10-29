230924e09dc1c035584c9476d07eb7f7011e003c
Nick Meyer <meyern4@rpi.edu>
2019-10-28 23:15:06 -0400

    Finished database login
    Made it so that login attempts are checked against the database

--- index.php
+++ index.php
@@ -12,13 +12,22 @@
         $_SESSION['loginUsername'] = "-1";
     }
     else if($_SERVER["REQUEST_METHOD"] == "POST") {
-        $username = $password = $error = "";
-
-        $username = validate($_POST['username']);
-        $password = validate($_POST['password']);
-        $data = file_get_contents("users.txt");
-        $data = explode("\n", $data);
+        $db = new mysqli('localhost', 'root', 'cows', 'userdata');
+        $username = mysqli_real_escape_string($db, $_POST['username']);
+        $password = mysqli_real_escape_string($db, $_POST['password']);
+        
+        if (empty($username)) { array_push($errors, "Username is required"); }
+        if (empty($password)) { array_push($errors, "Password is required"); }
+        
+        $user_check_query = "SELECT * FROM user WHERE username='$username' OR email='$email' LIMIT 1";
+        $result = mysqli_query($db, $user_check_query);
+        $user = mysqli_fetch_assoc($result);
+        
         $validationPassed = FALSE;
+
+        if ($user) { // if user exists
+          $validationPassed = TRUE;
+        }
 
         foreach($data as $values) {
             $loginInfo = explode(":", $values);
