961e74525d087d25a145c617bd1c44d749c0ec8f
schans2 <schantz.samuel@gmail.com>
2019-10-30 10:16:30 -0400

    Starting register validation

--- Login/login.php
+++ Login/login.php
@@ -1,12 +1,9 @@
 <?php
     session_start();
     if($_SERVER["REQUEST_METHOD"] == "POST") {
-        $db = new mysqli('localhost', 'root', 'cows', 'peapods');
+        $db = new mysqli('localhost', 'root', 'Techie0110', 'peapods');
         $username = mysqli_real_escape_string($db, $_POST['username']);
         $password = mysqli_real_escape_string($db, $_POST['password']);
-        
-        if (empty($username)) { array_push($errors, "Username is required"); }
-        if (empty($password)) { array_push($errors, "Password is required"); }
         
         $user_check_query = "SELECT * FROM userdata WHERE username='$username' LIMIT 1";
         $result = mysqli_query($db, $user_check_query);
--- Login/register.php
+++ Login/register.php
@@ -1,36 +1,31 @@
     <?php
       session_start();
       $data = $username = $password = $firstName = $lastName = $email = "";
-      $errors = array();
+      $error = "";
 
       if($_SERVER["REQUEST_METHOD"] == "POST") {
-        $db = new mysqli('localhost', 'root', 'cows', 'peapods');
+        $db = new mysqli('localhost', 'root', 'Techie0110', 'peapods');
         $username = mysqli_real_escape_string($db, $_POST['username']);
         $password = mysqli_real_escape_string($db, $_POST['password']);
         $firstName = mysqli_real_escape_string($db, $_POST['firstname']);
         $lastName = mysqli_real_escape_string($db, $_POST['lastname']);
         $email = mysqli_real_escape_string($db, $_POST['email']);
         
-        if (empty($username)) { array_push($errors, "Username is required"); }
-        if (empty($email)) { array_push($errors, "Email is required"); }
-        if (empty($password)) { array_push($errors, "Password is required"); }
+        if (empty($username)) { $error += "2"; }
+        if (empty($email)) { $error += "3"; }
+        if (empty($password)) { $error += "4"; }
         
         $user_check_query = "SELECT * FROM userdata WHERE username='$username' OR email='$email' LIMIT 1";
         $result = mysqli_query($db, $user_check_query);
         $user = mysqli_fetch_assoc($result);
         
         if ($user) { // if user exists
-          if ($user['username'] === $username) {
-            array_push($errors, "Username already exists");
-          }
-      
-          if ($user['email'] === $email) {
-            array_push($errors, "Email already exists");
-          }
+          if ($user['username'] === $username) { $error += "5"; }
+          if ($user['email'] === $email) { $error += "6"; }
         }
       
         // Finally, register user if there are no errors in the form
-        if (count($errors) == 0) {
+        if (strlen($error) == 0) {
             $password = md5($password);//encrypt the password before saving in the database
       
             $query = "INSERT INTO userdata (lastName, firstName, username, password, email) 
@@ -39,7 +34,7 @@
             $_SESSION['errors'] = "";
         }
         else {
-          $_SESSION['errors'] = implode(", ", $errors);
+          $_SESSION['errors'] = $error;
         }
         $db->close();
       }
