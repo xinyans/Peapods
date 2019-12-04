e8b0b725236da91dad3d26fb10c5cbe7b3f913f6
Nick Meyer <meyern4@rpi.edu>
2019-12-04 12:32:57 -0500

    Fixed test file

--- test-connect.php
+++ test-connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "cows";
-$database = "peapods"
+$database = "peapods";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
