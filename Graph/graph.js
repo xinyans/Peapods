46b3f24f02a1bbb4b088af0d8b351ce20d55d125
Nick Meyer <meyern4@rpi.edu>
2019-12-04 18:13:40 -0500

    Minor Graph Updates
    Minor updates to the graph plugin

--- Graph/graph.js
+++ Graph/graph.js
@@ -1,77 +1,178 @@
 var data = {
-    "minx": 0,
-    "maxx": 10,
-    "miny": 0,
-    "maxy": 8,
-    "minz": 0,
-    "maxz": 8,
-    "gcount": 4,
-	"set": [
-        {
-            "x": 5,
-            "y": 0,
-            "z": 0,
-            "g": 2,
-            "c":0
-        },
-        {
-		    "x": 5,
-            "y": 2,
-            "z": 8,
-            "g": 3,
-            "c":0
-        },
-        {
-		    "x": 5,
-            "y": 2,
-            "z": 3,
-            "g": 1,
-            "c":0
-        },
-        {
-		    "x": 5,
-            "y": 5,
-            "z": 3,
-            "g": 4,
-            "c":0
-        },
-        {
-		    "x": 0,
-            "y": 3,
-            "z": 2,
-            "g": 2,
-            "c":0
-        },
-        {
-		    "x": 10,
-            "y": 0,
-            "z": 1,
-            "g": 1,
-            "c":0
-        },
-        {
-		    "x": 1,
-		    "y": 2,
-            "z": 9,
-            "g": 2,
-            "c":0
-        },
-        {
-		    "x": 1,
-            "y": 3,
-            "z": 7,
-            "g": 1,
-            "c":0          
-        },
-        {
-		    "x": 4,
-		    "y": 8,
-            "z": 5,
-            "g": 1,
-            "c":0
-        }
+    "data": [
+      {
+        "name": "a a",
+        "contact": "a",
+        "g": 0,
+        "c": -1,
+        "answers": [],
+        "data": [
+          0.83,
+          0.37,
+          0.44,
+          0.36,
+          0.23,
+          0.95,
+          0.81,
+          0.46,
+          0.73,
+          0.11
+        ]
+      },
+      {
+        "name": "b a",
+        "contact": "a",
+        "g": 1,
+        "c": -1,
+        "answers": [],
+        "data": [
+          0.35,
+          0.89,
+          0.54,
+          0.55,
+          0.77,
+          0.42,
+          0.66,
+          0.8,
+          0.81,
+          0.84
+        ]
+      },
+      {
+        "name": "c a",
+        "contact": "a",
+        "g": 2,
+        "c": -1,
+        "answers": [],
+        "data": [
+          0.36,
+          0.83,
+          0.04,
+          0.03,
+          0.86,
+          0.72,
+          0.6,
+          0.94,
+          0.33,
+          0.95
+        ]
+      },
+      {
+        "name": "d a",
+        "contact": "a",
+        "g": 2,
+        "c": -1,
+        "answers": [],
+        "data": [
+          0.01,
+          0.7,
+          0.12,
+          0.11,
+          0.44,
+          0.12,
+          0.51,
+          0.96,
+          0.5,
+          0.36
+        ]
+      },
+      {
+        "name": "e a",
+        "contact": "a",
+        "g": 3,
+        "c": -1,
+        "answers": [],
+        "data": [
+          0.02,
+          0.08,
+          0.18,
+          0.57,
+          0.67,
+          0.15,
+          0.96,
+          0.29,
+          0.97,
+          0.77
+        ]
+      },
+      {
+        "name": "f a",
+        "contact": "a",
+        "g": 0,
+        "c": -1,
+        "answers": [],
+        "data": [
+          0.9,
+          0.76,
+          0.1,
+          0.11,
+          0.19,
+          0.79,
+          0.86,
+          0.57,
+          0.46,
+          0.49
+        ]
+      },
+      {
+        "name": "g a",
+        "contact": "a",
+        "g": 1,
+        "c": -1,
+        "answers": [],
+        "data": [
+          0.08,
+          0.98,
+          0.88,
+          0.49,
+          0.63,
+          0.73,
+          0.41,
+          0.7,
+          0.52,
+          0.43
+        ]
+      },
+      {
+        "name": "h a",
+        "contact": "a",
+        "g": 0,
+        "c": -1,
+        "answers": [],
+        "data": [
+          0.93,
+          0.91,
+          0.87,
+          0.26,
+          0.01,
+          0.62,
+          0.82,
+          0.71,
+          0.8,
+          0.1
+        ]
+      },
+      {
+        "name": "i a",
+        "contact": "a",
+        "g": 3,
+        "c": -1,
+        "answers": [],
+        "data": [
+          0.65,
+          0.51,
+          0.46,
+          0.18,
+          0.21,
+          0.07,
+          0.23,
+          0.64,
+          0.07,
+          0.41
+        ]
+      }
     ]
-}
+  }
 
 function getRandomColor(){
     r = Math.floor(Math.random() * 255);
@@ -95,46 +196,41 @@ function setupCanvas(canvas) {
 }
 
 //Takes a dataset and columns that are not valid and returns the top three sdev column keys
-function topThreeSdev(data, donotuse){
+function topThreeSdev(data){
     variances = [];
-    for(key in data.set[0]){
-        c = true;
-        for(k = 0; k < donotuse.length; k++)
-            if(key == donotuse[k])
-                c = false;
-        if(c)
-            variances.push([key,sdev(data, key)]);
+    for(index = 0; index < data["data"][0]["data"].length; index++){
+        variances.push([index,sdev(data, index)]);
     }
     //Sorting variances largest to smallest
     variances.sort(function(a,b){return (a[1] < b[1]) ? 1 : -1;});
     //Returning the key for those variances
-    return [variances[0][0],variances[1][0],variances[2][0]];
+    return [variances[0][0], variances[1][0], variances[2][0]];
 }
 
 //Given a dataset and a column name this will calculate the sdev for the column
-function sdev(data, colName){
+function sdev(data, index){
     //Calculating the mean
     total = 0;
-    for(i = 0; i < data.set.length; i++)
-        total += data.set[i][colName];
-    mean = total/data.set.length;
+    for(i = 0; i < data["data"].length; i++)
+        total += data["data"][i]["data"][index];
+    mean = total/data["data"].length;
     //Calculating the sum of variances
     total = 0;
-    for(i = 0; i < data.set.length; i++)
-        total += Math.pow(data.set[i][colName] - mean, 2);
+    for(i = 0; i < data["data"].length; i++)
+        total += Math.pow(data["data"][i]["data"][index] - mean, 2);
     //Calculating sdev from variance
-    return Math.sqrt(total/data.set.length);
+    return Math.sqrt(total/data["data"].length);
 }
 
 //Returns the minimum and maximum values for a given column
 function minmax(data, key){
     min = Number.MAX_VALUE;
     max = Number.MIN_VALUE;
-    for(i = 0; i < data.set.length; i++){
-        if(data.set[i][key] < min)
-            min = data.set[i][key];
-        if(data.set[i][key] > max)
-            max = data.set[i][key];
+    for(i = 0; i < data["data"].length; i++){
+        if(data["data"][i]["data"][key] < min)
+            min = data["data"][i]["data"][key];
+        if(data["data"][i]["data"][key] > max)
+            max = data["data"][i]["data"][key];
     }
     return[min, max];
 }
@@ -146,24 +242,39 @@ function drawGraph(canvas, ctx, d, offsetx, offsety, dataSet, degx, degy, xaxis,
     //Clear any previous content in canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    // minx = 0;
+    // maxx = 1;
+    // miny = 0;
+    // maxy = 1;
+    // minz = 0;
+    // maxz = 1;
 
     temp = minmax(data, xaxis);
-    minx = temp[0] - 1;
-    maxx = temp[1] + 1;
+    minx = temp[0] - .1;
+    maxx = temp[1] + .1;
     temp = minmax(data, yaxis);
-    miny = temp[0] - 1;
-    maxy = temp[1] + 1;
+    miny = temp[0] - .1;
+    maxy = temp[1] + 0.1;
     temp = minmax(data, zaxis);
-    minz = temp[0] - 1;
-    maxz = temp[1] + 1;
+    minz = temp[0] - 0.1;
+    maxz = temp[1] + 0.1;
+
+
+    groupCount = 0;
+    for(i = 0; i < data["data"].length; i++){
+        if(data["data"][i]["g"] > groupCount)
+            groupCount = data["data"][i]["g"];
+    }
     //Retrieve bufferzones from the data
-
-
+    colors = new Array(groupCount + 1);
+    for(i = 0; i < colors.length; i++){
+        colors[i] = getRandomColor();
+    }
     //Getting increments which represent the size of one unit relative to the size of the canvas
-    xinc = Math.floor(d / (maxx - minx + 1));
-    yinc = Math.floor(d / (maxy - miny + 1));
+    xinc = Math.floor(d / (maxx - minx));
+    yinc = Math.floor(d / (maxy - miny));
     zinc = Math.floor(Math.sqrt(Math.pow((d * Math.cos(degx)),2) + Math.pow(d * Math.sin(degy),2)) / (maxz - minz + 1));
-    radius = Math.min(yinc, xinc);
+    radius = 4;
 
     //Setting the background colors of sections ----------->
 
@@ -273,30 +384,27 @@ function drawGraph(canvas, ctx, d, offsetx, offsety, dataSet, degx, degy, xaxis,
     ctx.stroke();   
     ctx.closePath(); 
 
-    //Placing data on the board -------------------------------------------------------------->
-    groupCount = dataSet.gcount;
-    colors = new Array(groupCount);
-
-    for(i = 0; i < groupCount; i++){
-        colors[i] = getRandomColor();
-    }
+    //Placing data on the board -------------------------------------------------------------
 
     // console.log("degx: " + (degx/2)/Math.PI * 180 + " degy: " + degy/Math.PI * 180);
-    for(i = 0; i < dataSet.set.length; i++){
-        item = dataSet.set[i];
-        x = d - ((item[xaxis] - minx) * xinc) + (((item[zaxis] - minz) * Math.sin(degx/2)) * zinc);
-        y = ((item[yaxis] - miny) * yinc) + ((item[zaxis] - minz) * Math.sin(degy) * zinc);
-        c = item[group] - 1;
-        if(item[color] == 0){
-            item[color] = colors[c];
+    for(i = 0; i < dataSet["data"].length; i++){
+        item = dataSet["data"][i];
+        x = d - ((item["data"][xaxis] - minx) * xinc) + (((item["data"][zaxis] - minz) * Math.sin(degx/2)) * zinc);
+        y = ((item["data"][yaxis] - miny) * yinc) + ((item["data"][zaxis] - minz) * Math.sin(degy) * zinc);
+        c = item["g"];
+        if(item["g"] == -1){
+            item["c"] = "#000000";
         }
-        ctx.fillStyle = item[color];
+        else {
+            item["c"] = colors[item["g"]];
+        }
+        ctx.fillStyle = item["c"];
         ctx.beginPath();
-        if(item[zaxis] == 0){
+        if(item["data"][zaxis] == 0){
             ctx.arc(offsetx + x, offsety + d - y, radius, 0, 2 * Math.PI);
         }
         else {
-            ctx.arc(offsetx + x, offsety + d - y, Math.ceil(radius/Math.sqrt(item[zaxis])), 0, 2 * Math.PI);
+            ctx.arc(offsetx + x, offsety + d - y, Math.ceil(radius * item["data"][zaxis]) + radius, 0, 2 * Math.PI);
         }
         ctx.fill();   
         ctx.closePath(); 
@@ -333,10 +441,10 @@ function drawGraph(canvas, ctx, d, offsetx, offsety, dataSet, degx, degy, xaxis,
 $.fn.graph = function(dataSet, donotuse, color, group, degx, degy){
 
     //Retrieves the axes of the graph that have highest sdev
-    axis = topThreeSdev(dataSet, donotuse);
+    axis = topThreeSdev(dataSet);
 
     //Sorting data by z axis
-    dataSet.set.sort(function(a, b){return (a[axis[0]] < b[axis[0]]) ? 1 : -1;});
+    // dataSet.set.sort(function(a, b){return (a[axis[0]] < b[axis[0]]) ? 1 : -1;});
 
 
     canvas = this[0];
@@ -346,9 +454,9 @@ $.fn.graph = function(dataSet, donotuse, color, group, degx, degy){
     d = ctx.canvas.height;
 
     //Size of canvas
-    offsetx = 0;
+    offsetx = 0.1 * d;
     offsety = 0.3 * d;
-    d *= 0.7;
+    d *= 0.5;
 
     drawGraph(canvas, ctx, d, offsetx, offsety, dataSet, degx, degy, axis[2], axis[1], axis[0], "g", "c");
 
@@ -398,6 +506,6 @@ $.fn.graph = function(dataSet, donotuse, color, group, degx, degy){
 }
 
 window.onload = function() {
-    $("#graph").graph(data, ["c", "g"], "c", "g", Math.PI/4, Math.PI/8);
+    $("#graph").graph(data, ["c", "g", "name", "answers", "contact"], "c", "g", Math.PI/4, Math.PI/8);
 }
 
