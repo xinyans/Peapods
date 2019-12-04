89bd0853d5f9a067b72a7549d40ebf1ade88b439
Nick Meyer <meyern4@rpi.edu>
2019-12-04 12:28:36 -0500

    Test File

--- test-connect.php
+++ test-connect.php
@@ -2,9 +2,10 @@
 $servername = "localhost";
 $username = "root";
 $password = "cows";
+$database = "peapods"
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $database);
 
 // Check connection
 if ($conn->connect_error) {
